c3d42c3ac3a8d2bfd83538fb1ab679b32425c3a5
R1chard8 <115303013+R1chard8@users.noreply.github.com>
2024-02-06 23:10:17 +0200

    Update StudentManagementSystem.cpp

--- StudentManagementSystem.cpp
+++ StudentManagementSystem.cpp
@@ -1,17 +1,12 @@
-//
-// File:        StudentManagementSystem.cpp
-// Description: Student Management System
-// Author:      Potop Richard
-// Github:      https://github.com/richard01000/HW2
-//
+#include <iostream>                 // Author:          Potop Richard
+#include <string>                   // Description:     Student Management System
+#include <list>                     // Github:          https://github.com/richard01000/HW2
 
-#include <iostream>
-#include <string>
-#include <list>
+int userChoice = 0;//this together with mainMenuLoopTexts must be placed somewhere else
 
 // mainMenuLoopTexts is for all the loop text displayed in the program.
 struct mainMenuLoopTexts {//must complete with variants
-    int userChoice = 0;
+    
     std::string textStart = "|===========================================|\n|Welcome to TUM's student management system!|\n|===========================================|\n\nWhat do you want to do?\n1 - General operations \n2 - Faculty operations\n3 - Student operations\n\n4 - Quit Program\n\nyour input> ";
     std::string textGeneralOperations = "\n\n|==================|\n|General operations|\n|==================|\n\nWhat do you want to do?\n\n1 - create faculty\n2 - display faculties\n3 - search student and show faculty\n4 - display all faculties of a field\n\n5 - Back\n6 - Quit Program\n\nyour input> ";
     std::string textFacultyOperations = "\n\n|==================|\n|Faculty operations|\n|==================|\n\nWhat do you want to do?\n\n1 - display enroled students\n2 - display graduated stundents\n3 - check if student belongs to faculty\n\n4 - Back\n5 - Quit Program\n\nyour input> ";
@@ -25,12 +20,12 @@ struct mainMenuLoopTexts {//must complete with variants
 mainMenuLoopTexts txt;//object to call anything from mainMenuLoopTexts
 
 class student {
-    std::string firstName = "First Name: ";
-    std::string lastName = "Last Name: ";
-    std::string email = "Email: ";
-    std::string enrollmentDate = "Enrollment date(dd/mm/yyyy): ";
-    std::string dateOfBirth = "Date of birth(dd/mm/yyyy): ";
-    
+    public:
+    std::string firstName;
+    std::string lastName;
+    std::string email;
+    std::string enrollmentDate;
+    std::string dateOfBirth;
 };
 class faculty {
     std::string name = "Name: ";
@@ -49,41 +44,25 @@ class faculty {
     };
 };
 class generalOperations{
-    std::list<std::string> faculties;
     public:
+    std::list<std::string> faculties;
     generalOperations(){// for now it works to input and output elements into a list now the only thing left here is to make more lists like this bitch
         std::cout<<"Testing for now --> ";
-        std::cin>>txt.userChoice;
-        if(txt.userChoice==1 || txt.userChoice==2){
-            //if(txt.userChoice==1){
+        std::cin>>userChoice;
+        if(userChoice==1){
             std::string listElement;    // AAAAAAAAAAAA dosent work because is in a IF statement and all this bullshit only works if (userChoice == 1)
             std::cout<<"Enter faculty: ";
             std::cin.ignore();// if i dont use cin.ingnore(); then the program is bitching and is skiping the getline(); ending the program
             getline(std::cin, listElement);
             faculties.push_back(listElement);
-            //}
-            //if(txt.userChoice==2){
             //this if for showing the list of faculties
             for(std::string a : faculties){// the variable (string a) takes the value of the components inside faculties one by one and goes trough loop (range based loop77)
                std::cout<<a<<std::endl;
             }
-        //}
         }
-
-    
     }
 };
 int main() {
     generalOperations();
-    generalOperations();
-    //
-    //  test loop texts
-    //
-    //  std::cout<<txt.textStart;
-    //  std::cout<<txt.textGeneralOperations;
-    //  std::cout<<txt.textFacultyOperations;
-    //  std::cout<<txt.textStudentOperations;
-    //
-    
     return 0;
 }
