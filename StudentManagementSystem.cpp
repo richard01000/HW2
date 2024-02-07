23f9d02a7a8920655eef2f0892b6b8ef28a8027b
R1chard8 <115303013+R1chard8@users.noreply.github.com>
2024-02-07 21:52:50 +0200

    Update StudentManagementSystem.cpp

--- StudentManagementSystem.cpp
+++ StudentManagementSystem.cpp
@@ -1,25 +1,37 @@
 #include <iostream>                 // Author:          Potop Richard
 #include <string>                   // Description:     Student Management System
-#include <list>//instead of list must use <vector>                     // Github:          https://github.com/richard01000/HW2
+#include <vector>                   // Github:          https://github.com/richard01000/HW2
 
-int userChoice = 0;//this together with mainMenuLoopTexts must be placed somewhere else
+#define OUT(var) std::cout<<var<<std::endl;
+
+int mainMenuChoice = 0;//this together with mainMenuLoopTexts must be placed somewhere else
 
 // mainMenuLoopTexts is for all the loop text displayed in the program.
-struct mainMenuLoopTexts {//must complete with variants
-    
-    std::string textStart = "|===========================================|\n|Welcome to TUM's student management system!|\n|===========================================|\n\nWhat do you want to do?\n1 - General operations \n2 - Faculty operations\n3 - Student operations\n\n4 - Quit Program\n\nyour input> ";
-    std::string textGeneralOperations = "\n\n|==================|\n|General operations|\n|==================|\n\nWhat do you want to do?\n\n1 - create faculty\n2 - display faculties\n3 - search student and show faculty\n4 - display all faculties of a field\n\n5 - Back\n6 - Quit Program\n\nyour input> ";
-    std::string textFacultyOperations = "\n\n|==================|\n|Faculty operations|\n|==================|\n\nWhat do you want to do?\n\n1 - display enroled students\n2 - display graduated stundents\n3 - check if student belongs to faculty\n\n4 - Back\n5 - Quit Program\n\nyour input> ";
-    std::string textStudentOperations = "\n\n|==================|\n|Student operations|\n|==================|\n\nWhat do you want to do?\n\n1 -";
+int mainMenuLoopTexts(){//must complete with variants
+    std::cout << "|===========================================|\n|Welcome to TUM's student management system!|\n|===========================================|\n\nWhat do you want to do?\n1 - General operations \n2 - Faculty operations\n3 - Student operations\n\n4 - Quit Program\n\nyour input> ";
+    std::cin>>mainMenuChoice;
+    switch (mainMenuChoice){//switch to manage all the text that could be accessed in the main menu
+    case 1:
+        std::cout<<"\n\n|==================|\n|General operations|\n|==================|\n\nWhat do you want to do?\n\n1 - create faculty\n2 - display faculties\n3 - search student and show faculty\n4 - display all faculties of a field\n\n5 - Back\n6 - Quit Program\n\nyour input> ";
+        break;
+    case 2:
+        std::cout<<"\n\n|==================|\n|Faculty operations|\n|==================|\n\nWhat do you want to do?\n\n1 - display enroled students\n2 - display graduated stundents\n3 - check if student belongs to faculty\n\n4 - Back\n5 - Quit Program\n\nyour input> ";
+        break;
+    case 3:
+        std::cout<<"\n\n|==================|\n|Student operations|\n|==================|\n\nWhat do you want to do?\n\n1 -";
+        break;
+    default:
+        std::cout<<"Error incorrect input pick between 1 and 3";
+    }
     // this shit is crazy all this must be in general operations
     std::string textCreateFaculty;
     std::string searchStundentFaculty;
     std::string displayUniversityFaculties;
     std::string displayAllFacultiesBelongingToAField;
-};
-mainMenuLoopTexts txt;//object to call anything from mainMenuLoopTexts
+    return 0;// i dont fucking know why this is here it gives warning without it
+}
 
-class student {
+class studentData {
     public:
     std::string firstName;
     std::string lastName;
@@ -28,9 +40,9 @@ class student {
     std::string dateOfBirth;
 };
 class faculty {
-    std::string name = "Name: ";
-    std::string abbreviation = "Abbreviation: ";
-    std::list<std::string> students {//must find a way to work with this bitch together with enumeration
+    std::string facultyName;
+    std::string facultyAbbreviation;
+    std::vector<std::string> students {//must find a way to work with this bitch together with enumeration
         "1",
         "2",
         "3"
@@ -45,12 +57,12 @@ class faculty {
 };
 class generalOperations{
     public:
-    std::list<std::string> faculties;//     AAAAAAAAAAAAAAAAAAAAAAAAAAAA must remove list because its not random acces memory list AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    std::vector<std::string> faculties;
     generalOperations(){// for now it works to input and output elements into a list now the only thing left here is to make more lists like this bitch
         std::cout<<"Testing for now --> ";
-        std::cin>>userChoice;
-        if(userChoice==1){
-            std::string listElement;    // AAAAAAAAAAAA dosent work because is in a IF statement and all this bullshit only works if (userChoice == 1)
+        std::cin>>mainMenuChoice;
+        if(mainMenuChoice==1){
+            std::string listElement;    // AAAAAAAAAAAA dosent work because is in a IF statement and all this bullshit only works if (mainMenuChoice == 1)
             std::cout<<"Enter faculty: ";
             std::cin.ignore();// if i dont use cin.ingnore(); then the program is bitching and is skiping the getline(); ending the program
             getline(std::cin, listElement);
@@ -62,7 +74,7 @@ class generalOperations{
         }
     }
 };
-int main() {
-    generalOperations();
+int main(){
+    mainMenuLoopTexts();
     return 0;
 }
